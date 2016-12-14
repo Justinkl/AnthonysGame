c77d7ee5ea83bd257c10cb8461e47522188c92dd
justinkl <justinleroyklosinski@gmail.com>
2016-12-13 17:00:42 -0700

    new switch and buttons

---
+++ Anthony's 2670 Game/Assets/InstructionSwitch.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstructionSwitch : MonoBehaviour {
+
+
+	public int instructions;
+	// Use this for initialization
+	void Start () {
+		instructions = 5;
+		Narrator ();
+	
+	}
+	
+	// Update is called once per frame
+	public void Narrator () {
+		switch (instructions) {
+		case 5:
+			StartCoroutine (Pause ());
+			print ("Looks like the zookeeper just quit.");
+			break;
+		case 4:
+			StartCoroutine (Pause ());
+			print ("Guess he just couldn't take the noise.");
+			break;
+		case 3:
+			StartCoroutine (Pause ());
+			print ("Each of the animals have a noise they make...");
+			break;
+		case 2:
+			StartCoroutine (Pause ());
+			print ("Try clicking and pushing arrow keys to see what the animals say.");
+			break;
+		case 1:
+			StartCoroutine (Pause ());
+			print ("Don't get into to trouble now, okay?");
+			break;
+		}
+	}
+
+	IEnumerator Pause ()
+	{
+		yield return new WaitForSeconds (4);
+		instructions--;
+		Narrator ();
+	}
+}
